c34bd4dcd45dad7078151f4e27434321177c053a
Thet Minn Htun <59432845+thetminnhtun-scm@users.noreply.github.com>
2022-07-31 18:29:40 +0630

    Laravel UI

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->created_at->toFormattedDateString() }}</td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="100%">No user.</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+
+            {{ $users->links() }}
+        </div>
+    </div>
+</div>
+
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\LocalizationController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('welcome', function() {
+Route::get('/', function() {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('users', function() {
+    $users = User::paginate(10);
+
+    return view('users.index', compact('users'));
 });
